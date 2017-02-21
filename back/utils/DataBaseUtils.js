ec1e059710bf64aeeec3114f2cb6002fa2fd9af6
Павлухин Илья <ilya.pavluhin@actimind.com>
2017-02-21 13:15:01 +0300

    Authorization in progress

--- back/utils/DataBaseUtils.js
+++ back/utils/DataBaseUtils.js
@@ -5,8 +5,12 @@
 import mongoose from "mongoose";
 import config from '../etc/config.json';
 import '../models/Team';
+import '../models/User'
+import crypto from 'crypto';
 
 const Team = mongoose.model('Team');
+const User = mongoose.model('User');
+
 
 export function setUpConnection() {
     mongoose.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`);
@@ -26,4 +30,35 @@ export function createTeam(data) {
 
 export function deleteTeam(id) {
     return Team.findById(id).remove();
+}
+
+export function createUser(userData){
+    const user = new Team({
+        name: userData.name,
+        email: userData.email,
+        password: hash(userData.password)
+    });
+    return user.save();
+}
+
+export function getUser(id) {
+    return User.findOne(id)
+}
+
+export function checkUser(userData) {
+    return User
+        .findOne({email: userData.email})
+        .then(function(doc){
+            if ( doc.password == hash(userData.password) ){
+                console.log("User password is ok");
+                return Promise.resolve(doc)
+            } else {
+                return Promise.reject("Error wrong")
+            }
+        })
+}
+
+function hash(text) {
+    return crypto.createHash('sha1')
+        .update(text).digest('base64')
 }
