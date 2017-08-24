9ed84bad0379abfec274ba290a3dcb57d0a33c02
Павлухин Илья <ilya.pavluhin@actimind.com>
2017-08-24 16:20:55 +0300

    Improve data selector

--- front/js/modules/modals/dateSelector.js
+++ front/js/modules/modals/dateSelector.js
@@ -15,24 +15,33 @@ const SelectDateView = BaseModalView.extend({
 
   ui: {
     date: '.js-date',
-    closeBtn: '.js-closeBtn'
+    closeBtn: '.js-closeBtn',
+    submitBtn:'.js-submitBtn'
   },
 
   events: {
     'click @ui.closeBtn': 'close',
+    'click @ui.submitBtn': 'submit'
   },
 
   initialize: function (options) {
     this.options = options;
+    this.date = null;
   },
 
   onRender: function () {
     this.el.querySelector('.js-date').flatpickr({
       enableTime: true,
-      inline:true
+      inline:true,
+      minDate: "today",
+      onChange:this.changeDate.bind(this)
     })
   },
-
+  changeDate:function(event){
+    console.log('change', event[0].getTime()/1000);
+    this.date = event[0].getTime()/1000;
+    this.el.querySelector('.js-submitBtn').disabled = false;
+  },
   navigate: function () {
     channelGlobal.trigger('place:selected',this.model);
     channelGlobal.trigger('modal:close');
@@ -41,6 +50,13 @@ const SelectDateView = BaseModalView.extend({
   close: function () {
     channelGlobal.trigger('modal:close');
   },
+
+  submit: function () {
+    if(this.date){
+      channelGlobal.trigger('date:selected',this.date);
+      channelGlobal.trigger('modal:close');
+    }
+  }
 });
 
 export default SelectDateView;
