2bdcabd710d991aeae5845381025b2f5b3201858
Ilya <i.pavluhin@yandex.ru>
2017-10-10 00:08:12 +0300

    Add subseasons view, routes and db utils

--- front/js/modules/modals/newPlayer.js
+++ front/js/modules/modals/newPlayer.js
@@ -15,70 +15,77 @@ import ImageCropper from '../../widgets/imageCropper/imageCropper';
 let channelGlobal = Radio.channel('global');
 
 const ImageView = Marionette.View.extend({
-  template: require('../../../templates/modals/image.hbs')
+    template: require('../../../templates/modals/image.hbs')
 });
 
 const NewTeamView = BaseModalView.extend({
-  template: require('../../../templates/modals/newPlayer.hbs'),
+    template: require('../../../templates/modals/newPlayer.hbs'),
 
-  regions: {
-    imageRegion: '.js-imageRegion'
-  },
+    regions: {
+        imageRegion: '.js-imageRegion'
+    },
 
-  initialize: function (options) {
-    this.options = options;
-    this.collection = new Players();
-    this.model = new this.collection.model();
-    this.model.set('owner', options.team._id);
-    channelGlobal.off('image:selected');
-  },
+    initialize: function (options) {
+        this.options = options;
+        this.collection = new Players();
+        this.model = new this.collection.model();
+        this.model.set('owner', options.team._id);
+        channelGlobal.off('image:selected');
+    },
 
-  onRender: function () {
-    var bindings = ModelBinder.createDefaultBindings(this.el, 'name');
-    new ModelBinder().bind(this.model, this.el, bindings);
+    onRender: function () {
+        var bindings = ModelBinder.createDefaultBindings(this.el, 'name');
+        new ModelBinder().bind(this.model, this.el, bindings);
 
-    if (this.model.get('image')) {
-      this.showChildView('imageRegion', new ImageView({model: this.model}));
-    } else {
-      this.showUploader();
-    }
-  },
+        if (this.model.get('image')) {
+            this.showChildView('imageRegion', new ImageView({model: this.model}));
+        } else {
+            this.showUploader();
+        }
+    },
 
-  submit: function () {
-    this.cropper.getCroppedImage()
-      .then((image)=> {
-        this.model.set('image', image);
+    submit: function () {
+        if (this.cropper) {
+            this.cropper.getCroppedImage()
+                .then((image) => {
+                    this.model.set('image', image);
+                    this.saveNewPlayer();
+                })
+        } else {
+            this.saveNewPlayer();
+        }
+    },
+    saveNewPlayer() {
         this.collection.add(this.model);
         this.model.save()
-          .then(function () {
-            console.info('new player was created with owner', this.options.team.id);
-            channelGlobal.trigger('player:created');
-            channelGlobal.trigger('modal:close');
-          }.bind(this))
-          .catch(function (err) {
-            console.error(err);
-          })
-      })
-  },
+            .then(function () {
+                console.info('new player was created with owner', this.options.team.id);
+                channelGlobal.trigger('player:created');
+                channelGlobal.trigger('modal:close');
+            }.bind(this))
+            .catch(function (err) {
+                console.error(err);
+            })
+    },
 
-  showUploader: function () {
-    this.uploadView = null;
-    this.uploadView = new UploadView();
-    this.showChildView('imageRegion', this.uploadView);
-    channelGlobal.on('image:selected', this.showCropper, this);
-  },
+    showUploader: function () {
+        this.uploadView = null;
+        this.uploadView = new UploadView();
+        this.showChildView('imageRegion', this.uploadView);
+        channelGlobal.on('image:selected', this.showCropper, this);
+    },
 
-  showCropper: function (file) {
-    this.cropper = null;
-    this.cropper = new ImageCropper({file: file});
-    this.showChildView('imageRegion', this.cropper);
-    this.cropper.on('cropper:cancel', this.showUploader.bind(this));
-  },
+    showCropper: function (file) {
+        this.cropper = null;
+        this.cropper = new ImageCropper({file: file});
+        this.showChildView('imageRegion', this.cropper);
+        this.cropper.on('cropper:cancel', this.showUploader.bind(this));
+    },
 
-  showImage: function (url) {
-    this.model.set('image', url);
-    this.showChildView('imageRegion', new ImageView({model: this.model}));
-  }
+    showImage: function (url) {
+        this.model.set('image', url);
+        this.showChildView('imageRegion', new ImageView({model: this.model}));
+    }
 });
 
 export default NewTeamView;
