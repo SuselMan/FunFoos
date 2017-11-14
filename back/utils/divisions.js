c269f2a833609eefd55fef02cd179bee9db0cb92
Ilya <i.pavluhin@yandex.ru>
2017-11-15 00:08:26 +0300

    Calculate meetings and games for season

--- back/utils/divisions.js
+++ back/utils/divisions.js
@@ -8,54 +8,57 @@ import '../models/Division';
 const Division = mongoose.model('Division');
 
 export function changeDivision(req) {
-  return new Promise(function (resolve, reject) {
-    Division.findById(req.params.id, function (err, player) {
-      if (player) {
-        Division.update({_id: req.params.id}, {image: req.body.image})
-          .then(function (isOk) {
-            Division.findById(req.params.id)
-              .then(function (player) {
-                resolve(player);
-              })
-              .catch(function (err) {
-                console.error(err);
-                reject(err);
-              })
-          })
-          .catch(function (err) {
-            reject(err);
-          })
-      } else {
-        reject({status: 500, message: 'Division not found'});
-      }
+    return new Promise(function (resolve, reject) {
+        Division.findById(req.params.id, function (err, player) {
+            if (player) {
+                Division.update({_id: req.params.id}, {image: req.body.image})
+                    .then(function (isOk) {
+                        Division.findById(req.params.id)
+                            .then(function (player) {
+                                resolve(player);
+                            })
+                            .catch(function (err) {
+                                console.error(err);
+                                reject(err);
+                            })
+                    })
+                    .catch(function (err) {
+                        reject(err);
+                    })
+            } else {
+                reject({status: 500, message: 'Division not found'});
+            }
+        });
     });
-  });
 
 }
 
 
 export function listDivisions(req) {
-  if (req.query.season) {
-    return Division.find({season: req.query.season})
-  }
-  return Division.find();
+    if (req.query.season) {
+        return Division.find({season: req.query.season})
+    }
+    if (req.query.subseason) {
+        return Division.find({subseason: req.query.subseason})
+    }
+    return Division.find();
 }
 
 export function createDivision(data, user) {
-  if (user.isAdmin) {
-    const division = new Division(data);
-    return division.save();
-  } else {
-    return new Promise(function (resolve, reject) {
-      reject({
-        status: 403,
-        msg: 'Forbidden'
-      });
-    })
-  }
+    if (user.isAdmin) {
+        const division = new Division(data);
+        return division.save();
+    } else {
+        return new Promise(function (resolve, reject) {
+            reject({
+                status: 403,
+                msg: 'Forbidden'
+            });
+        })
+    }
 }
 
 export function deleteDivision(id) {
-  return Division.findById(id).remove();
+    return Division.findById(id).remove();
 }
 
--- back/utils/seasons.js
+++ back/utils/seasons.js
@@ -10,105 +10,149 @@ import config from '../etc/config.json';
 const Season = mongoose.model('Season');
 
 export function listSeasons(req) {
-  if (req && req.query.state) {
-    return Season.find({state: req.query.state})
-  }
-  return Season.find();
+    if (req && req.query.state) {
+        return Season.find({state: req.query.state})
+    }
+    return Season.find();
 }
 
 export function createSeason(data) {
-  const team = new Season({
-    name: data.name
-  });
+    const team = new Season({
+        name: data.name
+    });
 
-  return team.save();
+    return team.save();
 }
 
 export function getSeason(req) {
-  return new Promise(function(resolve, reject) {
-    Season.findById(req.params.id,function(err,team){
-      if(team){
-        resolve(team);
-      } else {
-        reject({status:500,message:'User not found'});
-      }
+    return new Promise(function (resolve, reject) {
+        Season.findById(req.params.id, function (err, team) {
+            if (team) {
+                resolve(team);
+            } else {
+                reject({status: 500, message: 'User not found'});
+            }
+        });
     });
-  });
 }
 
-function eachWithEach(teams, season) {
-  var meetings = [];
-  teams.forEach((item, i, arr) => {
-    for (let j = i + 1; j < teams.length; j++) {
-      meetings.push(
-        {
-          host: item._id,
-          guest: arr[j]._id,
-          owner: season
-        },
-        {
-          host: arr[j]._id,
-          guest: item._id,
-          owner: season
+function getMeetings(teams, season) {
+    var meetings = [];
+    teams.forEach((item, i, arr) => {
+        for (let j = i + 1; j < teams.length; j++) {
+            meetings.push(
+                {
+                    host: item._id,
+                    guest: arr[j]._id,
+                    owner: season
+                },
+                {
+                    host: arr[j]._id,
+                    guest: item._id,
+                    owner: season
+                }
+            );
         }
-      );
+    });
+    return meetings;
+}
+
+function getGames(meetings, structure, divisionID, seasonID) {
+    const games = [];
+    for (let i = 0; i < meetings.length; i++) {
+        for (let j = 0; j < structure.length; j++) {
+            games.push({
+                meeting: meetings[i]._id,
+                division: divisionID,
+                season: seasonID,
+                type: structure[j]
+            })
+        }
     }
-  });
-  return meetings;
+    return games;
 }
 
 
 export function changeSeason(req) {
-  if (req.body.state && req.body.state === 2) {
-    this.startSeason(req.params.id);
-  }
-  return new Promise(function (resolve, reject) {
-    Season.findById(req.params.id, function (err, season) {
-      if (season) {
-        Season.update({_id: req.params.id}, req.body)
-          .then(function (isOk) {
-            Season.findById(req.params.id)
-              .then(function (season) {
-                resolve(season);
-              })
-              .catch(function (err) {
-                console.error(err);
-                reject(err);
-              })
-          })
-          .catch(function (err) {
-            reject(err);
-          })
-      } else {
-        reject({status: 500, message: 'Season not found'});
-      }
+    if (req.body.state && req.body.state === 2) {
+        return this.calculateSeason(req.params.id)
+    }
+    return new Promise(function (resolve, reject) {
+        Season.findById(req.params.id, function (err, season) {
+            if (season) {
+                Season.update({_id: req.params.id}, req.body)
+                    .then(function (isOk) {
+                        Season.findById(req.params.id)
+                            .then(function (season) {
+                                resolve(season);
+                            })
+                            .catch(function (err) {
+                                console.error(err);
+                                reject(err);
+                            })
+                    })
+                    .catch(function (err) {
+                        reject(err);
+                    })
+            } else {
+                reject({status: 500, message: 'Season not found'});
+            }
+        });
     });
-  });
 
 }
 
-export function startSeason(id) {
-  console.info('Start season', id);
-  const structure = config.meetingStructire;
-  const games = [];
-  this.listTeams(null).then((teams) => {
-    if (teams && teams.length > 1) {
-      this.createMeetings(eachWithEach(teams, parseInt(id)))
-        .then((result) => {
-          for(let i = 0; i< result.length; i++){
-            for(let j = 0; j < structure.length; j++){
-              games.push({
-                meeting: result[i]._id,
-                season: id,
-                type: structure[j]
-              })
+export function calculateDivision(division, seasonID) {
+    console.log('calculateDivision', division);
+    const id = division._id;
+    return new Promise((resolve, reject) => {
+        const structure = division.meetingStructure;
+        const games = [];
+        this.listTeams({query: {division: id}}).then((teams) => {
+            if (teams && teams.length > 1) {
+                console.log('find teams', teams.length);
+                this.createMeetings(getMeetings(teams, parseInt(id)))
+                    .then((result) => {
+                        this.createGames(getGames(result, structure, id, seasonID))
+                            .then(() => {
+                                    resolve();
+                                }
+                            )
+                            .catch((err)=>{
+                                reject({msg: `Не удалось создать игры для встречи ${result._id}, "${err}"`, code: 500})
+                            })
+                    })
+                    .catch((err) => {
+                        reject({msg: `Не удалось создать встречи для дивизиона ${id}, "${err}"`, code: 500});
+                    })
+            } else {
+                reject({msg: `Слишком мало команд в дивизионе: ${id}`, code: 403})
             }
-          }
-          this.createGames(games)
-        })
-        .catch((err) => {
-          console.error('Something wrong', err);
-        })
-    }
-  });
+        });
+    })
+}
+
+export function calculateSeason(id) {
+    console.log('calculateSeason', id);
+    return new Promise((resolve, reject) => {
+        const promises = [];
+        const divisions = this.listDivisions({query: {season: id}});
+        divisions.then((result) => {
+            console.log('find divisions',result.length);
+            if (!result || !result.length) {
+                reject({msg: 'Не создано ни одного дивизиона', code: 403});
+            } else {
+                for (let i = 0; i < result.length; i++) {
+                    promises.push(this.calculateDivision(result[i], id));
+                }
+                Promise.all(promises)
+                    .then((res) => {
+                        resolve();
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    })
+            }
+        });
+    });
 }
